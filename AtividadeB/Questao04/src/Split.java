8ec618f9a759369d26a2c142a27924718f4b2777
Henrique Garcia <enriqe_garcia15@hotmail.com>
2017-11-09 15:15:23 -0300

    Respostas da Atividade B

--- AtividadeB/Questao04/src/Split.java
+++ AtividadeB/Questao04/src/Split.java
@@ -2,5 +2,25 @@ public class Split {
 
     private String nome;
     private double potencia;
-    
+
+    public Split(String nome, double potencia) {
+        this.nome = nome;
+        this.potencia = potencia;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getPotencia() {
+        return potencia;
+    }
+
+    public void setPotencia(double potencia) {
+        this.potencia = potencia;
+    }
 }
