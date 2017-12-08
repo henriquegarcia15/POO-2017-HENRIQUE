1c131dd79baf04e07d377967cda7e5d62212822e
Henrique Garcia <enriqe_garcia15@hotmail.com>
2017-12-07 23:21:49 -0300

    ..

--- AtividadeD/src/Exercicio06/Questao03/app/TestaAnimais.java
+++ AtividadeD/src/Exercicio06/Questao03/app/TestaAnimais.java
@@ -2,14 +2,25 @@ package Exercicio06.Questao03.app;
 
 import Exercicio06.Questao03.model.Animal;
 import Exercicio06.Questao03.model.Bovino;
+import Exercicio06.Questao03.model.Peixe;
 
 public class TestaAnimais {
     public static void main(String[] args) {
 
-        // Questão 04 - a) o problema é que está sendo requerido o nome de uma especie
-        //                 e não a quantidade
+        // Questão 04 - a) o problema é que Animal não herda de Bovino
+        //                 por isso que acontece o erro.
+        //
+        //                 Animal a = new Bovino();
+        //                 a.setProducaoLeite(5);
 
-        Animal a = new Bovino();
-        a.setEspecie();
+        // Solução
+        Bovino a = new Bovino();
+        a.setProducaoLeite(5);
+
+        // Questão 04 - b) não é possível devido à incompatibilidade de tipos
+        //                 Peixe p = new Animal();
+
+        //Solução
+        Peixe p = (Peixe) new Animal();
     }
 }
