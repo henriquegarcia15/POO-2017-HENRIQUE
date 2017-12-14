a476b2f547e60c44d5a590e8c7a37f8880490898
Henrique Garcia <enriqe_garcia15@hotmail.com>
2017-12-14 00:00:27 -0300

    ..

---
+++ AtividadeF/src/model/AtualizadorDeContas.java
@@ -0,0 +1,26 @@
+package model;
+
+public class AtualizadorDeContas {
+
+    private double saldoTotal = 0;
+    private double selic;
+
+    public AtualizadorDeContas(double selic) {
+        this.selic = selic;
+    }
+
+    public void roda(Conta c){
+        System.out.println("Saldo anterior: "+ c.getSaldo());
+        c.atualiza(this.getSelic());
+        this.saldoTotal += c.getSaldo();
+        System.out.println("Saldo final: "+ this.saldoTotal);
+    }
+
+    public double getSelic() {
+        return selic;
+    }
+
+    public double getSaldoTotal() {
+        return saldoTotal;
+    }
+}
---
+++ AtividadeF/src/model/ContaCorrente.java
@@ -0,0 +1,17 @@
+package model;
+
+public class ContaCorrente extends Conta {
+
+    public ContaCorrente() {
+    }
+
+    public void atualiza(double taxa){
+        super.atualiza(taxa * 2);
+    }
+
+    public void deposita(double valor){
+        this.saldo += valor - 0.10;
+    }
+
+
+}
